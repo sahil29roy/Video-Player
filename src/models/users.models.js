6cb649f49266471ab3e48a9a4c4517a7e95023c5
sahil <sahil29roy@gmail.com>
2024-04-12 21:27:35 +0530

    fix error at userMode

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -51,7 +51,7 @@ userSchema.pre('save',async function(next){
 
     if(!this.isModified('password')) return next();
 
-    this.password = bcrypt.hash(this.password,10);
+    this.password = await bcrypt.hash(this.password,10);
     next();
 })
 
