49e2b2502ebbcca26c82a9df261885239b00799e
sahil <sahil29roy@gmail.com>
2024-04-12 19:02:41 +0530

    add cloudinary util

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary";
+
+import fs from "fs"; 
+
+          
+cloudinary.config({ 
+  cloud_name: Process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try{
+        if(!localhost) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type : "auto"
+        }) 
+        console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+    }catch(err){
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+} 
+export {uploadOnCloudinary};
