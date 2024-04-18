36a2d86cff5727dac7370569ccabd5b65300e560
sahil <sahil29roy@gmail.com>
2024-04-18 13:15:07 +0530

    add routes for login and logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 // import {User} from "../models/users.models.js"
 const router = Router();
 
@@ -16,6 +21,10 @@ router.route("/register").post(
         }
     ]),
     registerUser);
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJwt,logoutUser)
 
 export default router;
 
