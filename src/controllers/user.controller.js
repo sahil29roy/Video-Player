78151ddbf6ac74da86ce6c46d99f18f1110c4ec5
sahil <sahil29roy@gmail.com>
2024-04-26 14:08:34 +0530

    add aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
 import { application } from "express";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -367,6 +368,56 @@ const getUserChannelProfile = asyncHandler( async(req,res)=>{
         new ApiResponse(200,channel[0],"User channel fetched sucessfully")
     )
 } )
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup : {
+                from : "videos",
+                localField:'watchHistory',
+                forignField : "_id",
+                as : "watchHistory",
+                pipeline:[
+                    {
+                        $lookup : {
+                            from : "users",
+                            localField : "owner",
+                            forignField : "_id",
+                            as : "owner",
+                            pipeline : [
+                                {
+                                    $project : {
+                                        fullName : 1,
+                                        username : 1,
+                                        avatar : 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields : {
+                            owner: {
+                                $first :"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully")
+    )
+})
 
 export {
     registerUser,
