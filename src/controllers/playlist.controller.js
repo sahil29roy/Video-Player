db4d3aeade1fbe297bcfa7f3eb157c914c691817
sahil <sahil29roy@gmail.com>
2024-04-29 12:31:09 +0530

    add aggregation on getPlaylistById

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,73 +1,73 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Playlist } from "../models/playlist.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
-    
-    if(!name){
-        throw new ApiError(400,"playlist name is required!")
+    const { name, description } = req.body
+
+    if (!name) {
+        throw new ApiError(400, "playlist name is required!")
     }
-    if(!description){
-        throw new ApiError(400,"Discription is required!")
+    if (!description) {
+        throw new ApiError(400, "Discription is required!")
     }
 
 
-    const playlist = await  Playlist.create(
+    const playlist = await Playlist.create(
         {
             name,
             description,
             owner: req.user?._id,
         }
     )
-     
+
     const createdPlaylist = await Playlist.findById(playlist._id);
 
-    if(!createdPlaylist){
-        throw new ApiError(400,"Playlist not created ")
+    if (!createdPlaylist) {
+        throw new ApiError(400, "Playlist not created ")
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createPlaylist,"playlist created successfully ")
+        new ApiResponse(200, createPlaylist, "playlist created successfully ")
     )
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    
-    if(!isValidObjectId(userId)){
-        throw new ApiError(400,"Invalid userId!")
+    const { userId } = req.params
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId!")
     }
 
     const playlist = Playlist.aggregate([
         {
-            $match : {
-                owner : mongoose.Types.ObjectId(userId)
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
             }
         },
         {
-            $lookup : {
-                from : "videos",
-                localField : "videos",
-                foreignField : "_id",
-                as : "videos"
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
             }
         },
         {
-            $addFields : {
-                totalVideos : {
-                    $size : "$videos"
+            $addFields: {
+                totalVideos: {
+                    $size: "$videos"
                 },
-                totalViews : {
-                    $sum : "videos.views"
+                totalViews: {
+                    $sum: "$videos.views"
                 }
             }
         },
         {
-            $project : {
+            $project: {
                 name: 1,
                 description: 1,
                 totalVideos: 1,
@@ -77,51 +77,93 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
         }
     ]);
 
-    if(!playlist){
+    if (!playlist) {
         throw new ApiError(400, "Playlist not found!")
     }
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(200,playlist,"Playlist fetched sucessfully")
-    )
+        .status(200)
+        .json(
+            new ApiResponse(200, playlist, "Playlist fetched sucessfully")
+        )
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    
-    if(!isValidObjectId(playlistId)){
+    const { playlistId } = req.params
+
+    if (!isValidObjectId(playlistId)) {
         throw new ApiError(400, " PlaylistId not found!")
     }
     const playlist = Playlist.findById(playlistId)
 
-    if(!playlist){
+    if (!playlist) {
         throw new ApiError(400, "Playlist does not exist ")
     }
     const playlistVideo = Playlist.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(playlistId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $match: {
+                "videos.isPublished": true,
+            },
+        },
+        {
+            $lookup : {
+                from : "users",
+                localField : "owner",
+                foreignField : "_id",
+                as : "owner"
+            }
+        },
+        {
+            $addFields : {
+                totalVideos : {
+                    $size : "$videos"
+                },
+                totalViews : {
+                    $sum : "$videos.views"
+                },
+                owner : {
+                    $first : "$owner"
+                }
+            }
+        },
+        {
+
+        }
         
     ])
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
+    const { playlistId, videoId } = req.params
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
+    const { playlistId, videoId } = req.params
     // TODO: remove video from playlist
 
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
+    const { playlistId } = req.params
     // TODO: delete playlist
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
+    const { playlistId } = req.params
+    const { name, description } = req.body
     //TODO: update playlist
 })
 
