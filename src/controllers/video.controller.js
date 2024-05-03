032bfa2d2f62f343756821ee91aa4f45300b394b
sahil <sahil29roy@gmail.com>
2024-05-03 23:04:37 +0530

    check userId and check

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,6 +10,13 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    if (!userId) {
+        throw new ApiError(400, "User Id not provided");
+      }
+    
+      if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid User Id");
+      }
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
