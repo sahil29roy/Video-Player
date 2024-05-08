aa05e8ca853282357cb767beab5f0e2430c3789d
sahil <sahil29roy@gmail.com>
2024-05-08 23:24:47 +0530

    remove comments

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -98,6 +98,7 @@ const getVideoById = asyncHandler(async (req, res) => {
     }
     
     
+    
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -11,8 +11,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
-
+router.use(verifyJWT);
 router
     .route("/")
     .get(getAllVideos)
