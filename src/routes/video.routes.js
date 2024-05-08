f257d9dc73d3ecfcaa6cb27a990ca8da5a3809b9
sahil <sahil29roy@gmail.com>
2024-05-08 23:21:21 +0530

    add all import to video router

---
+++ src/routes/video.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import {
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+  publishAVideo,
+  togglePublishStatus,
+  updateVideo,
+} from "../controllers/video.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
