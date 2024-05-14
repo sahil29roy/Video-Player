e715f09fcbbd896b0f6ceb49bc9d93dd4e99924e
sahil <sahil29roy@gmail.com>
2024-05-14 23:57:15 +0530

    add comment router

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { 
+  addComment,
+  updateComment,
+  deleteComment,
+} from "../controllers/comment.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/:videoId").post(addComment);
+
+router.route("/c/:commentId").patch(updateComment).delete(deleteComment);
+
+export default router;
