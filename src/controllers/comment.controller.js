21c488cbd7407f75b254f340718243dabe8be084
sahil <sahil29roy@gmail.com>
2024-05-14 23:42:13 +0530

    add update coment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -32,3 +32,46 @@ const addComment = asyncHandler(async (req, res) => {
         .status(200)
         .json(new ApiResponse(200, uploadComment, "Comment added successfully"));
 })
+
+
+const updateComment = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    if (!isValidObjectId(commentId)) {
+      throw new ApiError(400, "Comment id is not valid");
+  }
+
+  if (!content) {
+      throw new ApiError(400, "Content not provided");
+  }
+
+  const getComment = await Comment.findById(commentId);
+
+  if(reqq.user?._id.toString() !== getComment?.owner.toString()){
+    throw new ApiError(400, "User is not the owner of this comment");
+  }
+
+  const uploadComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content: content,
+      },
+    },
+    { new: true }
+  );
+  if (!uploadComment) {
+    throw new ApiError(500, "Failed to update comment");
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      uploadComment,
+      "Comment has been updated Successfully"
+    )
+  );
+})
