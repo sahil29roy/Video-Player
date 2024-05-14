ceeaaab41d2ffa3fbd4a542959242fd83be10ec9
sahil <sahil29roy@gmail.com>
2024-05-14 22:59:45 +0530

    fix error and formate document

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -6,29 +6,29 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 
-const addComment = asyncHandler(async (req,res)=> {
+const addComment = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
     const { content } = req.body;
-    
+
     if (!isValidObjectId(videoId)) {
         throw new ApiError(400, "videoId is not valid");
-      }
+    }
 
-      if (!content) {
+    if (!content) {
         throw new ApiError(400, "Content not provided");
-      }
+    }
 
-      const uploadComment = await Comment.create({
+    const uploadComment = await Comment.create({
         content,
-        video : videoId,
-        owner : req.user?._id
-      });
+        video: videoId,
+        owner: req.user?._id
+    });
 
-      if (!uploadComment) {
+    if (!uploadComment) {
         throw new ApiError(500, "Failed to upload the comment");
-      }
-    
-      return res
-    .status(200)
-    .json(new ApiResponse(200, uploadComment, "Comment added successfully"));
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, uploadComment, "Comment added successfully"));
 })
