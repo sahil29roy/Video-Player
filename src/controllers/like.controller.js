b95db9a8c8627c40f06aca7ecfe2a74b5741b315
sahil <sahil29roy@gmail.com>
2024-05-15 01:00:03 +0530

    toggle allready liked video

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,3 +4,28 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const toggleVideoLikes =  asyncHandler(async (req,res) =>{
+
+    const {videoId} =  req.params;
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video Id");
+      }
+
+    const alreadyLiked = await Like.findOne({
+        video : videoId,
+        likedBy : req.user?._id
+    })
+    if(alreadyLiked){
+        await Like.findByIdAndDelete(alreadyLiked?._id);
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {isLiked : false},
+                "video like removed sucessfully "
+            )
+        );
+    }
+})
